fdf1b5a2a441e04cb25847014523e6acd8bba8c7
SFKyoto <cauaakitoti@gmail.com>
2022-09-18 16:00:30 -0300

    Avatar pt.1

---
+++ Assets/Scripts/MultiPlayerGuessesManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using RiptideNetworking;
+
+public class MultiPlayerGuessesManager : GameGuessesManager
+{
+    //public NetworkClientManager networkClientManager;
+    private void Awake()
+    {
+        Debug.Log("multiplayer client started.");
+    }
+
+    public override void GenerateNewAnswer() { }
+
+    public override string GetCheckedAttempt(string currentGuess)
+    {
+        Debug.Log($"Multiplayer message: {currentGuess}");
+        //Debug.Log(networkClientManager);
+        //if (networkClientManager == null) SetConn();
+        //networkClientManager.SendMessage(currentGuess);
+
+        Message message = Message.Create(MessageSendMode.reliable, (ushort)ClientToServerId.wordGuess);
+        message.AddString(currentGuess);
+        NetworkClientManager.Singleton.Client.Send(message);
+
+        return "";
+    }
+
+}
